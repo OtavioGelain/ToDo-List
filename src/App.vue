0e8fcb6a1b953ed141ba5eb2f75532e8fc1c5470
OtavioGelain <otavioluizgelain@gmail.com>
2026-08-10 14:56:52 -0300

    add Tarefa component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,53 @@
 <script setup>
+    import Tarefa from './components/Tarefa.vue'
+    const data = {
+        tarefa: {
+            id: 1,
+            titulo: 'Estudar Vue.js',
+            descricao: 'Aprender os conceitos básicos do Vue.js e criar um projeto simples.',
+        }
+    }
 </script>
 
 <template>
-  
+    <div class="container">
+        <h1 class="mt-4">Lista de Tarefas</h1>
+        <div>
+            <p>Titulo da Tarefa</p>
+            <input type="text" class="form-control" >
+        </div>
+        <div>
+            <p>Descrição da Tarefa</p>
+            <input type="text" class="form-control" >
+        </div>
+        <button class="btn btn-primary">Adicionar Tarefa</button>
+    </div>
+  <Tarefa 
+    :titulo="data.tarefa.titulo" 
+    :descricao="data.tarefa.descricao" 
+    />
 </template>
+<style>
+    body{
+        background-color: #f8f9fa;
+        display: flex;
+        justify-content: center;
+        gap: 50px;
+    }
+    .container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+    }
+    .mt-4{
+        margin-top: 20px;
+        text-align: center;
+    }
+    .btn{
+        width: 150px;
+        margin-top: 10px;
+    }
+    
+</style>
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="tarefa">
+        <h4 class="tarefa-titulo">{{ titulo }}</h4>
+        <p class="tarefa-descricao">{{ descricao }}</p>
+        <button class="btn btn-primary">Concluir</button>
+    </div>
+</template>
+<script setup>
+    const props = defineProps({
+        titulo: String,
+        descricao: String
+    })
+</script>   
+<style>
+    .tarefa{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-color: aliceblue;
+        border-radius: 10px;
+        border: solid 1px #ccc;
+        width: 400px;
+        height: 150px;
+        text-align: center;
+        margin-top: 20px;
+    }
+    .btn{
+        width: 100px;
+    }
+
+</style>
+
